e76bd28e6fd92fb460c08b21d47687006d4a2e8a
tdahms <>
2011-10-24 14:44:49 +0000

    This commit was manufactured by cvs2svn to create tag 'help'.
    Sprout from master 2011-10-24 14:44:48 UTC tdahms 'first commit of skimming code (pp configuration)'
    Delete:
        HIN-10-006/efficiency/PlotEff_Quarkonia.C
        HiAnalysis/HiMuTrigEff/BuildFile
        HiAnalysis/HiMuTrigEff/plugins/BuildFile
        HiAnalysis/HiMuTrigEff/plugins/HiMuTrigEffAnalyzer.cc
        HiAnalysis/HiMuTrigEff/python/himutrigeffanalyzer_cfi.py
        HiAnalysis/HiMuTrigEff/test/himutrigeffanalyzer_cfg.py
        HiAnalysis/HiMuTrigEff/test/launch_himutrigeffJobs_allv2.sh
        HiAnalysis/HiMuTrigEff/test/launch_himutrigeffJobs_allv3.sh

--- HiAnalysis/HiMuTrigEff/python/himutrigeffanalyzer_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-mutrigeff = cms.EDAnalyzer('HiMuTrigEffAnalyzer',
-                           srcMuon = cms.InputTag("patMuonsWithTrigger"),
-                           primaryVertexTag = cms.InputTag("hiSelectedVertex"),
-                           maxAbsZ = cms.double(24.0),
-                           histFileName = cms.string("MuTrigEff_Histos.root"), 
-                           trigFilterName = cms.string("hltHIL2DoubleMu3L2Filtered")
-)
