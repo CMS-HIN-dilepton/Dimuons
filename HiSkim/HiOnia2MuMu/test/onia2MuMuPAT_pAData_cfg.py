c167ef8a0638de1016c9a9977dcf2aa86fde6a5d
tdahms <>
2013-01-23 20:23:31 +0000

    add VarParsing to cfg and setup scripts for caf

--- HiSkim/HiOnia2MuMu/test/onia2MuMuPAT_pAData_cfg.py
+++ HiSkim/HiOnia2MuMu/test/onia2MuMuPAT_pAData_cfg.py
@@ -2,9 +2,21 @@
 # https://twiki.cern.ch/twiki/bin/view/CMS/Onia2MuMuSamples
 
 import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
 
 # set up process
 process = cms.Process("Onia2MuMuPAT")
+
+# setup 'analysis'  options
+options = VarParsing.VarParsing ('analysis')
+
+# setup any defaults you want
+options.outputFile = 'onia2MuMuPAT.root'
+options.inputFiles = '/store/caf/user/tdahms/Data2013/pPb/ExpressSkims/Run_210498/pAMuonDimuon.root'
+options.maxEvents = -1 # -1 means all events
+
+# get and parse the command line arguments
+options.parseArguments()
 
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load("Configuration.StandardSequences.Reconstruction_cff")
@@ -42,15 +54,15 @@ onia2MuMuPAT(process, GlobalTag=process.GlobalTag.globaltag, MC=False, HLT="HLT"
 #process.onia2MuMuPatTrkTrk.resolvePileUpAmbiguity = False
 
 process.source.fileNames = cms.untracked.vstring(
-    '/store/caf/user/tdahms/Data2013/pPb/ExpressSkims/Run_210498/pAMuonDimuon.root'
+    options.inputFiles
     )
 
 # filter on lumisections
 #from HiSkim.HiOnia2MuMu.goodLumiSectionListHI_cfi import *
 #process.source.lumisToProcess = goodLumisToProcess
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-process.outOnia2MuMu.fileName = cms.untracked.string( 'onia2MuMuPAT.root' )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.maxEvents) )
+process.outOnia2MuMu.fileName = cms.untracked.string( options.outputFile )
 process.pACentrality_step = cms.Path(process.hltOniaHI*process.PAcollisionEventSelection*process.pACentrality)
 
 process.e = cms.EndPath(process.outOnia2MuMu)
