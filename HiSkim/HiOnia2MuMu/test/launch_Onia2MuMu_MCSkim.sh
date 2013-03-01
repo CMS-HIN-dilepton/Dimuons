6b15dd6fc361b962f0c2fd8b6f025d794707d554
tdahms <>
2013-03-01 13:40:55 +0000

    update MC cfg

--- HiSkim/HiOnia2MuMu/test/launch_Onia2MuMu_MCSkim.sh
+++ HiSkim/HiOnia2MuMu/test/launch_Onia2MuMu_MCSkim.sh
@@ -7,12 +7,12 @@ function command(){
 
 workDir=$PWD  
                                                                        
-logDir=${workDir}"/BATCHJOBS/MC_v7h0/"
+logDir=${workDir}"/BATCHJOBS/MC_v4h0/"
 command "mkdir -p $logDir"
 
-castorDirIn="/castor/cern.ch/cms/store/user/tdahms/HeavyIons/Onia/MC/v7/"
-castorDirOut="/castor/cern.ch/cms/store/user/tdahms/HeavyIons/Onia/MC/v7/Skims/h0/"
-command "rfmkdir -p $castorDirOut"
+castorDirIn="/store/group/phys_heavyions/dileptons/MC2013/5TeV/NonPromptJpsi/"
+castorDirOut="/store/group/phys_heavyions/dileptons/MC2013/5TeV/NonPromptJpsi/Skims/"
+command "cmsMkdir $castorDirOut"
 
 
 j=0
@@ -20,11 +20,15 @@ jobNb=""
 
 #for file in `nsls $castorDirIn`
 #for i in `seq 0 99`
-for fileList in `ls inputFilesMC_10?`
+
+#declare -a missingRuns=(0)
+#for i in `echo ${missingRuns[@]}`
+for fileList in `ls nonpromptJpsi_??`
 do
+#    fileList="upsilon1s_"$i
     echo $fileList;
-    # file="MC_PromptJpsi_FEVTDEBUGHLT_"${i}".root"
-    jobNb="10"${j};
+    # file="MC_PromptNonPromptJpsi_FEVTDEBUGHLT_"${i}".root"
+    jobNb=${j};
     let j=${j}+1;
     name="onia2MuMuPAT_MC_${jobNb}"
     outfilename="${name}.root"
@@ -39,36 +43,27 @@ function command(){
      eval \$@                                         
 }
 
-
-workDir="/afs/cern.ch/user/t/tdahms/scratch0/HeavyIons2010/CMSSW_3_9_9_patch1/src/HeavyFlavorAnalysis/Onia2MuMu/test/"
-logDir=${workDir}"/BATCHJOBS/MC_v2h0/"
-castorDirIn="/castor/cern.ch/cms/store/user/tdahms/HeavyIons/Onia/MC/v2/"
-castorDirOut="/castor/cern.ch/cms/store/user/tdahms/HeavyIons/Onia/MC/v2/Skims/h0/"
-
-cd /afs/cern.ch/user/t/tdahms/scratch0/HeavyIons2010/CMSSW_3_9_9_patch1/src/
+cd ${CMSSW_BASE}/src
 eval \`scramv1 runtime -sh\`
 cd -
-cp /afs/cern.ch/user/t/tdahms/scratch0/HeavyIons2010/CMSSW_3_9_9_patch1/src/HeavyFlavorAnalysis/Onia2MuMu/test/onia2MuMuPATHIMC_CAF_cfg.py .
-cp /afs/cern.ch/user/t/tdahms/scratch0/HeavyIons2010/CMSSW_3_9_9_patch1/src/HeavyFlavorAnalysis/Onia2MuMu/test/${fileList} .
+cp ${workDir}/onia2MuMuPAT_pAMC_cfg.py tmp.py
+cp ${workDir}/${fileList} .
 echo "before running cmsRun"
 ls -l
 
-command "cmsRun onia2MuMuPATHIMC_CAF_cfg.py inputFiles_clear inputFiles_load=${fileList} print 2>&1 | tee -a $logDir${name}.log"
-
+command "cmsRun tmp.py inputFiles_clear inputFiles_load=${fileList} print 2>&1 | tee -a $logDir${name}.log"
 echo "after running cmsRun"
 ls -l
 
-command "rfcp onia2MuMuPAT_MC.root ${castorDirOut}${outfilename}"
-command "rfcp tnpSta_MC.root ${castorDirOut}tnpSta_MC_${jobNb}.root"
-command "rfcp tnpMuID_MC.root ${castorDirOut}tnpMuID_MC_${jobNb}.root"
-command "rfcp tnpTrig_MC.root ${castorDirOut}tnpTrig_MC_${jobNb}.root"
-
+command "cmsStage onia2MuMuPAT_MC.root ${castorDirOut}${outfilename}"
 command "gzip -f $logDir${name}.log"         
+rm -f tmp.py
+rm -f ${fileList}
 
 EOF
 
-	chmod 755 job_Onia2MuMu_MCSkim_${jobNb}.sh
-	
-	bsub -q 1nd -J $name /afs/cern.ch/user/t/tdahms/scratch0/HeavyIons2010/CMSSW_3_9_9_patch1/src/HeavyFlavorAnalysis/Onia2MuMu/test/job_Onia2MuMu_MCSkim_${jobNb}.sh
+    chmod 755 job_Onia2MuMu_MCSkim_${jobNb}.sh
+    
+    bsub -q 2nd -J $name ${workDir}/job_Onia2MuMu_MCSkim_${jobNb}.sh
 
 done
--- HiSkim/HiOnia2MuMu/test/onia2MuMuPAT_pAMC_cfg.py
+++ HiSkim/HiOnia2MuMu/test/onia2MuMuPAT_pAMC_cfg.py
@@ -2,9 +2,22 @@
 # https://twiki.cern.ch/twiki/bin/view/CMS/Onia2MuMuSamples
 
 import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
 
 # set up process
 process = cms.Process("Onia2MuMuPAT")
+
+# setup 'analysis'  options
+options = VarParsing.VarParsing ('analysis')
+
+# setup any defaults you want
+options.inputFiles = '/store/group/phys_heavyions/dileptons/MC2013/5TeV/Upsilon1S/MC_Upsilon1S_FEVTSIM_0.root'
+options.outputFile = 'onia2MuMuPAT_MC.root'
+
+options.maxEvents = -1 # -1 means all events
+
+# get and parse the command line arguments
+options.parseArguments()
 
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load("Configuration.StandardSequences.Reconstruction_cff")
@@ -14,6 +27,9 @@ process.GlobalTag.globaltag = 'STARTHI53_V17::All'
 
 # Common offline event selection
 process.load("HeavyIonsAnalysis.Configuration.collisionEventSelection_cff")
+
+# pile up rejection
+process.load('Appeltel.RpPbAnalysis.PAPileUpVertexFilter_cff')
 
 # Centrality for pPb
 process.load('RecoHI.HiCentralityAlgos.HiCentrality_cfi')
@@ -42,26 +58,27 @@ onia2MuMuPAT(process, GlobalTag=process.GlobalTag.globaltag, MC=True, HLT="HLT",
 process.patMuonSequence = cms.Sequence(
 #    process.hltOniaHI *
     process.PAcollisionEventSelection *
+    process.pileupVertexFilterCutGplus * 
+    process.pACentrality_step *
     process.genMuons *
     process.patMuonsWithTriggerSequence
     )
 
-
+process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
 process.source.fileNames = cms.untracked.vstring(
-    '/store/group/phys_heavyions/dileptons/MC2013/5TeV/Upsilon1S/MC_Upsilon1S_FEVTSIM_0.root'
+    options.inputFiles
     )
 
 # filter on lumisections
 #from HiSkim.HiOnia2MuMu.goodLumiSectionListHI_cfi import *
 #process.source.lumisToProcess = goodLumisToProcess
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-process.outOnia2MuMu.fileName = cms.untracked.string( 'onia2MuMuPAT.root' )
-process.pACentrality_step = cms.Path(process.PAcollisionEventSelection*process.pACentrality)
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.maxEvents) )
+process.outOnia2MuMu.fileName = cms.untracked.string( options.outputFile )
 
 process.e = cms.EndPath(process.outOnia2MuMu)
 
-process.schedule = cms.Schedule(process.pACentrality_step,
-                                process.Onia2MuMuPAT,process.e)
+process.schedule = cms.Schedule(process.Onia2MuMuPAT,
+                                process.e)
 
 
