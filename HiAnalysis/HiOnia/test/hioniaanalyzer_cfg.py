0dd025002fb006f2b3fa89fc6bc3e1e16c1e6964
tdahms <>
2013-06-17 14:01:58 +0000

    fix centrality cfg

--- HiAnalysis/HiOnia/test/hioniaanalyzer_cfg.py
+++ HiAnalysis/HiOnia/test/hioniaanalyzer_cfg.py
@@ -23,15 +23,15 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 #process.GlobalTag.globaltag = 'GR_E_V33::All' # express reco
 process.GlobalTag.globaltag = 'GR_P_V43D::All' # prompt reco
 
-#from HeavyIonsAnalysis.Configuration.CommonFunctions_cff import *
-#overrideCentrality(process)
+from HeavyIonsAnalysis.Configuration.CommonFunctions_cff import *
+overrideCentrality(process)
 
-#process.HeavyIonGlobalParameters = cms.PSet(
-#    centralityVariable = cms.string("HFtowersPlusTrunc"),
-#    nonDefaultGlauberModel = cms.string(""),
-#    centralitySrc = cms.InputTag("pACentrality"),
-#    pPbRunFlip = cms.untracked.uint32(211313)
-#    )
+process.HeavyIonGlobalParameters = cms.PSet(
+    centralityVariable = cms.string("HFtowersPlusTrunc"),
+    nonDefaultGlauberModel = cms.string(""),
+    centralitySrc = cms.InputTag("pACentrality"),
+    pPbRunFlip = cms.untracked.uint32(211313)
+    )
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.maxEvents) )
 
