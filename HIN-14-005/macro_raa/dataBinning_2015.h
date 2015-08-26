026df29ae9aea036b83d4ac123a09409dcc58831
Mihee <mihee.jo@cern.ch>
2015-08-26 22:10:01 +0200

    1 PbPb efficiency bin fixed

--- HIN-14-005/macro_raa/dataBinning_2015.h
+++ HIN-14-005/macro_raa/dataBinning_2015.h
@@ -6,6 +6,16 @@ double ppLumi    = 5400;
 int npartShift=2;
 
 /// ------------------------------ binning 
+// 40-100, 40-20, 20-0
+const int nBinsNpart3 = 3;
+double binsNpart3[nBinsNpart3]   = {32.75, 158.5653, 308.3853};
+double binsNpart3Err[nBinsNpart3]= {0,0,0};
+double binsNpart3X[nBinsNpart3]  = {10,10,10};
+
+double adTaa5[nBinsNpart3]       = {0.85, 6.95, 18.84};
+double adTaa5Err[nBinsNpart3]    = {0.11, 0.58, 0.99}; // this is wrong. to be fixed later
+double adDeltaCent5[nBinsNpart3] = {0.6,  0.2,  0.2};
+
 // 50-100, 50-40, 40-30,30-20, 20-10, 10-0
 const int nBinsNpart6 = 6;
 double binsNpart6[nBinsNpart6]    = {22.0709, 86.2622, 129.9835, 187.1470, 261.4178,355.3528};
